a64b5df43927965fc8a8acb10b04a3344a5c3095
Charles Wooley <cwooley89@gmail.com>
2017-10-24 13:31:56 -0400

    Built out linear regression model in python and made graphs

--- mySimpleLinear.py
+++ mySimpleLinear.py
@@ -15,11 +15,11 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -28,3 +28,38 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+                            
+#Fitting Simple Linear Regression Model to Training set
+# *** Comes with Feature Scaling out of box ***
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression(n_jobs=-1)
+regressor.fit(X_train, y_train)
+
+
+#Predicting the Test set results
+#Create vector of predictions for the test set
+y_pred = regressor.predict(X_test)
+
+#Visualizing the Training set Results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
